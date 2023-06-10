7a75cdbf2f66bfe463f4141a24b935e5e3f54cd7
SanguchoMela <melany01-03@hotmail.com>
2023-06-10 11:18:04 -0500

    Métodos para calculo del area de 4 figuras tridimensionales

--- src/Figuras_Tridimensionales.java
+++ src/Figuras_Tridimensionales.java
@@ -8,4 +8,26 @@ public class Figuras_Tridimensionales {
     private float lado_pir, altu_pir;
     //variables para esfera
     private float radio_esf;
+
+
+
+
+    //Métodos para calcular el area de figuras tridimensionales
+    //Cubo
+    private double area_cubo(){
+        return 6 * Math.pow(this.lado_cub,2);
+    }
+    //Cilindro
+    private double area_cilindro(){
+        return 2 * this.radio_cil * this.pi * (this.radio_cil + this.altura_cil);
+    }
+    //Piramide
+    private double area_piramide(){
+        return this.lado_pir * (this.lado_pir + (Math.sqrt((4 * Math.pow(this.altu_pir,2)) + Math.pow(this.lado_pir,2))));
+    }
+    //Esfera
+    private double area_esfera(){
+        return 4 * this.pi * Math.pow(radio_esf,2);
+    }
+
 }
