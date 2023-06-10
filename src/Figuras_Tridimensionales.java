8293853ef356ab0429c05e72fd420cee54dd482c
arianacatani2009 <mayerlicatani@gmail.com>
2023-06-10 12:41:43 -0500

    constructores setter y getter

--- src/Figuras_Tridimensionales.java
+++ src/Figuras_Tridimensionales.java
@@ -9,8 +9,74 @@ public class Figuras_Tridimensionales {
     //variables para esfera
     private float radio_esf;
 
+//constructores
+     public Figuras_Tridimensionales(float lado_cub, float radio_cil, float altura_cil, double pi, float lado_pir, float altu_pir,float radio_esf){
+         this.pi= pi;
+         this.lado_cub= lado_cub;
+         this.lado_pir= lado_pir;
+         this.altu_pir= altu_pir;
+         this.radio_cil= radio_cil;
+         this.radio_esf=radio_esf;
+         this.altura_cil=altura_cil;
+}
+//setter and getter
 
 
+    private float getLado_cub() {
+        return lado_cub;
+    }
+
+    private void setLado_cub(float lado_cub) {
+        this.lado_cub = lado_cub;
+    }
+
+    private float getRadio_cil() {
+        return radio_cil;
+    }
+
+    private void setRadio_cil(float radio_cil) {
+        this.radio_cil = radio_cil;
+    }
+
+    private float getAltura_cil() {
+        return altura_cil;
+    }
+
+    private void setAltura_cil(float altura_cil) {
+        this.altura_cil = altura_cil;
+    }
+
+    private double getPi() {
+        return pi;
+    }
+
+    private void setPi(double pi) {
+        this.pi = pi;
+    }
+
+    private float getLado_pir() {
+        return lado_pir;
+    }
+
+    private void setLado_pir(float lado_pir) {
+        this.lado_pir = lado_pir;
+    }
+
+    private float getAltu_pir() {
+        return altu_pir;
+    }
+
+    private void setAltu_pir(float altu_pir) {
+        this.altu_pir = altu_pir;
+    }
+
+    private float getRadio_esf() {
+        return radio_esf;
+    }
+
+    private void setRadio_esf(float radio_esf) {
+        this.radio_esf = radio_esf;
+    }
 
     //Métodos para calcular el area de figuras tridimensionales
     //Cubo
