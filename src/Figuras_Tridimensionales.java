77cc79e183c004106944f0066afd1243e4cf6df8
Gilmar <gilmarmoralesriva2003@gmail.com>
2023-06-09 20:46:40 -0500

    VARIABLES DE LAS CUATRO FIGURAS

--- src/Figuras_Tridimensionales.java
+++ src/Figuras_Tridimensionales.java
@@ -1,2 +1,11 @@
 public class Figuras_Tridimensionales {
+    //Variables para el cubo
+    private float lado_cub;
+    //variables para el cilindro
+    private float radio_cil, altura_cil;
+    private double pi=3.1416;
+    //variables para la piramide
+    private float lado_pir, altu_pir;
+    //variables para esfera
+    private float radio_esf;
 }
